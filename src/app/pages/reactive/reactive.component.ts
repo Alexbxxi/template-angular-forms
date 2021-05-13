ac8b2ffb2cffcb9d0f9b54e1a1cbef2e0bb87d24
Alejandro Bautista Jiménez <alejandrobj1996@gmail.com>
2021-05-13 17:00:14 -0500

    Avance formulario reactivo

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from 'src/app/services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,7 +11,9 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private validadores: ValidadoresService) {
+
     this.crearFormulario();
     this.cargarDataAlFormulario();
   }
@@ -23,37 +26,55 @@ export class ReactiveComponent implements OnInit {
   }
 
   get nombreNoValido() {
-    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
   }
 
   get apellidoNoValido() {
-    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
   }
 
   get correoNoValido() {
-    return this.forma.get('correo').invalid && this.forma.get('correo').touched
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
+  get usuarioNoValido() {
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched;
   }
 
   get calleNoValida() {
-    return this.forma.get('direccion.calle').invalid && this.forma.get('direccion.calle').touched
+    return this.forma.get('direccion.calle').invalid && this.forma.get('direccion.calle').touched;
   }
 
   get coloniaNoValida() {
-    return this.forma.get('direccion.colonia').invalid && this.forma.get('direccion.colonia').touched
+    return this.forma.get('direccion.colonia').invalid && this.forma.get('direccion.colonia').touched;
+  }
+
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido() {
+    const pass1= this.forma.get('pass1').value;
+    const pass2= this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 ) ? false : true;
   }
 
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
-      apellido: ['', Validators.required],
+      apellido: ['', [Validators.required, this.validadores.noBautista]],
       correo: ['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      usuario: ['', ,this.validadores.existeUsuario],
+      pass1: ['', Validators.required],
+      pass2: ['', Validators.required],
       direccion: this.fb.group({
         calle: ['', Validators.required],
         colonia: ['', Validators.required]
       }),
-      pasatiempos: this.fb.array([
-        
-      ])
+      pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
     });
   }
 
@@ -64,6 +85,8 @@ export class ReactiveComponent implements OnInit {
       nombre: 'Alejandro',
       apellido: 'Bautista',
       correo: 'asdf@gmail.com',
+      pass1: '123',
+      pass2: '123',
       direccion: {
           calle: 'Toronto',
           colonia: 'Canadá'
--- src/app/services/validadores.service.ts
+++ src/app/services/validadores.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+interface ErrorValidate {
+  [s:string]: boolean
+}
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +12,48 @@ import { Injectable } from '@angular/core';
 export class ValidadoresService {
 
   constructor() { }
+
+  existeUsuario( control: FormControl ): Promise<ErrorValidate> | Observable<ErrorValidate> {
+
+    if (!control.value) {
+      return Promise.resolve(null);
+    }
+
+    return new Promise(( resolve, reject ) => {
+      setTimeout(() => {
+        if ( control.value === 'strider' ){ 
+        resolve({ existe: true });        
+        } else {
+          resolve: null
+        }
+      }, 3500);
+    });
+  };
+
+  noBautista( control: FormControl ): {[s:string]: boolean} {
+    if ( control.value?.toLowerCase() === 'bautista2' ) {
+      return {
+        noBautista: true
+      };  
+    }
+    return null;  
+  };
+
+  passwordsIguales( pass1Name: string, pass2Name: string ){
+    return ( formGroup: FormGroup ) => {
+
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+
+      // console.log(pass1Control);
+      // console.log(pass2Control);
+      
+      if ( pass1Control.value === pass2Control.value ) {
+        pass2Control.setErrors(null); 
+      } else {
+        pass2Control.setErrors({ noEsIgual: true });
+      }
+    }
+  };
+
 }
